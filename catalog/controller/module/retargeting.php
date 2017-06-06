67001f2f477cfe6f9a0518537f6da84e78c24c62
CarolPelu <pelucaroltheo@gmail.com>
2017-06-06 16:17:52 +0300

    fix some arrays to fit older PHP versions

--- catalog/controller/module/retargeting.php
+++ catalog/controller/module/retargeting.php
@@ -457,12 +457,12 @@ class ControllerModuleRetargeting extends Controller {
             $product_categories = $product_categories->rows; // Get all the subcategories for this product. Reorder its numerical indexes to ease the breadcrumb logic
             $encoded_product_name = htmlspecialchars($product_details['name']);
             $decoded_product_url = htmlspecialchars_decode($product_url);
-            $rootCat = array([
+            $rootCat = array(array(
                 'id' => 'Root',
                 'name' => 'Root',
                 'parent' => false,
                 'breadcrumb' => []
-            ]);
+            ));
             /* Send the base info */
             $data['sendProduct'] = "
                                     var _ra = _ra || {};
@@ -512,17 +512,17 @@ class ControllerModuleRetargeting extends Controller {
                 $preCat = array();
                 foreach ($catDetails as $productCategory) {
                     if (isset($productCategory['parent_id']) && ($productCategory['parent_id'] == 0)) {
-                        $preCat = array([
+                        $preCat = array(array(
                             'id' => $productCategory['category_id'],
                             'name' => htmlspecialchars($productCategory['name']),
                             'parent' => false,
                             'breadcrumb' => []
-                        ]);
+                        ));
 
                     } else {
 
                         $breadcrumbDetails =  $this->model_catalog_category->getCategory($productCategory['parent_id']);
-                        $preCat = array([
+                        $preCat = array(array(
                             'id' => (int)$productCategory['category_id'],
                             'name' => htmlspecialchars($productCategory['name']),
                             'parent' => 'Root',
@@ -532,7 +532,7 @@ class ControllerModuleRetargeting extends Controller {
                                 'name' => 'Root',
                                 'parent' => false    
                             ]]
-                        ]);
+                        ));
                         
                     }
                 }
